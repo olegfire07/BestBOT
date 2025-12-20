7ae9982d2e8945a057575b3ea4efc29cb836d21c
Oleg Zhukov <oleg@Olegs-MacBook-Pro.local>
2025-12-20 20:12:34 +0300

    Update Web App: 2025-12-20 20:12:34

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // Service Worker for offline support
-const VERSION = '2322';
+const VERSION = '2323';
 const CACHE_NAME = `sklad-bot-${VERSION}`;
 const swPath = self.location.pathname.replace(/service-worker\.js.*$/, '');
 const BASE_PATH = swPath.endsWith('/') ? swPath : `${swPath}/`;
